08c997d55b19b227ecfa0aef550a4faaffa92622
DimLav <dllddlld123@gmail.com>
2017-03-27 00:29:52 +0300

    task six

---
+++ project euler №6.py
@@ -0,0 +1,2 @@
+for number in range(1, 1000000000):
+            if (number % 2 != 0) or (number % 3 != 0) or (number % 4 != 0) or (number % 5 != 0) or (number % 6 != 0) or (number % 7 != 0) or (number % 8 != 0) or (number % 9 != 0) or (number % 10 != 0) or (number % 11 != 0) or (number % 12 != 0) or (number % 13 != 0) or (number % 14 != 0) or (number % 15 != 0) or (number % 16 != 0) or (number % 17 != 0) or (number % 18 != 0) or (number % 19 != 0) or (number % 20 != 0):
